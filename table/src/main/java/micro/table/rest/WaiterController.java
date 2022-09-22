3b01e5e149e779b757278400be059b527a138df7
Marcell Sárkány <marci.sarkany@gmail.com>
2022-09-22 20:34:48 +0200

    Implement deliver & payment controllers

--- table/src/main/java/micro/table/rest/WaiterController.java
+++ table/src/main/java/micro/table/rest/WaiterController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/waiter")
@@ -23,25 +24,30 @@ public class WaiterController {
 
     @PutMapping("/payment/{tableId}")
     public void payment(@PathVariable String tableId) {
-        // TODO ...
+        List<OrderItemsState> orders = orderItemRepository.getAll(tableId);
+        List<String> orderIds = new ArrayList<>();
+        orders.forEach(orderItemsState -> {
+            orderIds.addAll(orderItemsState.getOrders().stream()
+                    .map(OrderItem::getOrderId)
+                    .collect(Collectors.toList()));
+        });
+        orderItemRepository.modifyState(orderIds, OrderStateEnum.paid);
     }
 
     @PostMapping("/order/{tableId}") // TODO: tableId needed?
     public void order(@PathVariable String tableId,
                       @RequestBody List<OrderItem> orders) {
-
         List<OrderItemsState> list = new ArrayList<>();
         list.add(OrderItemsState.builder()
                         .orders(orders)
                         .state(OrderStateEnum.ordered)
                         .build());
         orderItemRepository.add(tableId, list);
-
     }
 
     @PutMapping("/delivery")
-    public void deliver(List<String> orderIds) {
-        // TODO ...
+    public void deliver(@RequestBody List<String> orderIds) {
+        orderItemRepository.modifyState(orderIds, OrderStateEnum.delivered);
     }
 
     @GetMapping("/statuses/{tableId}")
