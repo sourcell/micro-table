ead8c6ada0bf0dba68bece2a73bbea6754d31521
Marcell Sárkány <marci.sarkany@gmail.com>
2022-09-22 16:54:21 +0200

    Add cors config

--- table/src/main/java/micro/table/TableApplication.java
+++ table/src/main/java/micro/table/TableApplication.java
@@ -1,13 +1,32 @@
 package micro.table;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
+@EnableAutoConfiguration
 @SpringBootApplication
 public class TableApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TableApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }
