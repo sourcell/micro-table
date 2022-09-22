c56e054b2f66c933f2647523df3db0d13e8eaf76
Marcell Sárkány <marci.sarkany@gmail.com>
2022-09-22 16:46:11 +0200

    Add OrderItemRepository skeleton

---
+++ table/src/main/java/micro/table/store/repository/OrderItemRepository.java
@@ -0,0 +1,50 @@
+package micro.table.store.repository;
+
+import micro.table.store.service.OrderItemsState;
+import micro.table.store.service.OrderStateEnum;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderItemRepository {
+
+    // TODO: is the hash key orderId or tableId?
+    //       currently: tableId
+    Map<String, List<OrderItemsState>> orders = new HashMap<>();
+
+    void add(String tableId, List<OrderItemsState> orders) {
+        List<OrderItemsState> ordersByTableId = this.orders.get(tableId);
+        if (ordersByTableId == null) {
+            this.orders.put(tableId, new ArrayList<>(orders));
+        } else {
+            ordersByTableId.addAll(orders);
+        }
+    }
+
+    // TODO: wtf?
+    void modifyState(List<String> orderId, OrderStateEnum state) {
+
+    }
+
+    void delete(List<String> orderId) {
+        List<OrderItemsState> allOrders = new ArrayList<>();
+        orders.forEach((key, value) -> {
+            allOrders.addAll(value);
+        });
+
+        allOrders.forEach(orderItemsState -> {
+            orderItemsState.getOrders().removeIf(orderItem -> orderId.contains(orderItem.getOrderId()));
+        });
+    }
+
+    void clear(String tableId) {
+        orders.put(tableId, new ArrayList<>());
+    }
+
+    List<OrderItemsState> getAll(String tableId) {
+        return orders.get(tableId);
+    }
+
+}
