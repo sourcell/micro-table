3d8f75bc2d91f996d3206986aff4e6f48efba0ed
Marcell Sárkány <marci.sarkany@gmail.com>
2022-09-22 12:49:50 +0200

    Add OrderItemsState

---
+++ table/src/main/java/micro/table/store/service/OrderItemsState.java
@@ -0,0 +1,12 @@
+package micro.table.store.service;
+
+import micro.table.datamodel.OrderItem;
+
+import java.util.List;
+
+public class OrderItemsState {
+
+    private List<OrderItem> orders;
+    private OrderStateEnum state;
+
+}
