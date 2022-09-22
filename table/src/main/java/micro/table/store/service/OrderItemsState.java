c2f5d699eb21ddd7004205909dd9a57b528eb693
Marcell Sárkány <marci.sarkany@gmail.com>
2022-09-22 19:01:46 +0200

    Add builder to OrderItemsState

--- table/src/main/java/micro/table/store/service/OrderItemsState.java
+++ table/src/main/java/micro/table/store/service/OrderItemsState.java
@@ -1,15 +1,24 @@
 package micro.table.store.service;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import micro.table.datamodel.OrderItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class OrderItemsState {
 
     private List<OrderItem> orders = new ArrayList<>();
     private OrderStateEnum state;
+
+    @Builder
+    public OrderItemsState(List<OrderItem> orders, OrderStateEnum state) {
+        this.orders = orders;
+        this.state = state;
+    }
 
 }
