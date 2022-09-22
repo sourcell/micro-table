9ba9d91af8af945326830a5a81d8b4168296e382
Marcell Sárkány <marci.sarkany@gmail.com>
2022-09-22 12:12:54 +0200

    Generate project

---
+++ table/src/test/java/micro/table/TableApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
