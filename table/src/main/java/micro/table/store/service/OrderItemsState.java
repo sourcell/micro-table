a351fc87d0c1e7d415ee763c1178fb38467f5c96
Marcell Sárkány <marci.sarkany@gmail.com>
2022-09-22 16:45:47 +0200

    Modify OrderItemsState

--- table/src/main/java/micro/table/store/service/OrderItemsState.java
+++ table/src/main/java/micro/table/store/service/OrderItemsState.java
@@ -1,12 +1,15 @@
 package micro.table.store.service;
 
+import lombok.Data;
 import micro.table.datamodel.OrderItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class OrderItemsState {
 
-    private List<OrderItem> orders;
+    private List<OrderItem> orders = new ArrayList<>();
     private OrderStateEnum state;
 
 }
